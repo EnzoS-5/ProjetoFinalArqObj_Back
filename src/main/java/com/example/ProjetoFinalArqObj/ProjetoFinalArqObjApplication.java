dcf9f0a853a66c3f56e397f42b414bad9ffedcff
Enzo Santoro <ez.santoro@gmail.com>
2026-04-27 14:09:46 -0300

    Initial commit

---
+++ src/main/java/com/example/ProjetoFinalArqObj/ProjetoFinalArqObjApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoFinalArqObj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalArqObjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalArqObjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjetoFinalArqObj/ProjetoFinalArqObjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoFinalArqObj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalArqObjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
